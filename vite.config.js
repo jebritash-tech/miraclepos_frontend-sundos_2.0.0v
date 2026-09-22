087a4c044a071cac33838a47183602324bb51a92
jebritash-tech <jebritash@gmail.com>
2026-09-22 15:56:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { resolve } from 'path';
 export default defineConfig({
-  base: '/miraclepos_frontend_2.0.0v/',
+  base: '/miraclepos_frontend-sundos_2.0.0v/',
   global: 'globalThis',
   resolve: {
     alias: {
